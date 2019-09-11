83a624c41c3f435b1c4fa57523665bfbd2f253a9
rickymarinsalda <esample.com>
2019-09-11 12:40:07 +0200

    Ho fatto 2/3 della parte blu

--- dml/area_blu.sql
+++ dml/area_blu.sql
@@ -39,43 +39,118 @@ FOREIGN KEY (agriturismo,`numero stanza`) REFERENCES Stanza(agriturismo,`numero 
 FOREIGN KEY (tipo) REFERENCES `Tipologia letto`(tipo)
 );
 
-CREATE TABLE `Prenotazione stanza` (
-
-
-
-);
 CREATE TABLE `Prenotazione stanze` (
+`data arrivo`				DATE NOT NULL,
+`utente`					CHAR(16) NOT NULL,
+`data partenza` 			DATE NOT NULL,
+`timestamp prenotazione` 	TIMESTAMP NOT NULL,
 
+PRIMARY KEY 				pk1(`data arrivo`,`utente`),
 
 );
+
+CREATE TABLE `Prenotazione stanza` (
+`numero stanza` 	INT UNSIGNED NOT NULL,
+`agriturismo`		INT UNSIGNED NOT NULL,
+`data arrivo`		DATE NOT NULL,
+`utente`			CHAR(16),
+
+PRIMARY KEY 		pk1(`numero stanza`,`agriturismo`,`data arrivo`,`utente`),
+
+
+-- Chiave esterna riferita a Stanza
+FOREIGN KEY (`numero stanza`,agriturismo) REFERENCES Stanza(`numero stanza`,agriturismo),
+
+-- Chiave esterna riferita a Prenotazione stanze
+FOREIGN KEY (`data arrivo`,utente) REFERENCES `Prenotazione stanze`(`data arrivo`,utente)
+
+);
+
+CREATE TABLE `Servizio aggiuntivo` (
+`nome` 			CHAR(60) PRIMARY KEY,
+`prezzo` 		INT UNSIGNED NOT NULL
+);
+
 CREATE TABLE `Prenotazione servizio` (
+`data arrivo`			DATE NOT NULL,
+`utente`				CHAR(16) NOT NULL,
+`giorno` 				DATE NOT NULL,
+`servizio aggiuntivo` 	CHAR(60) NOT NULL,
 
+PRIMARY KEY 			pk1(`data arrivo`,`utente`,`giorno`,`servizio aggiuntivo`),
 
+-- Chiave esterna riferita al servizio aggiuntivo
+FOREIGN KEY (`servizio aggiuntivo`) REFERENCES `Servizio aggiuntivo`(nome),
+
+-- Chiave esterna riferita al servizio aggiuntivo
+FOREIGN KEY (`data arrivo`,utente) REFERENCES `Prenotazione stanze`(`data arrivo`,utente)
 );
 CREATE TABLE `Utente` (
-
-
+`codice carta` 			CHAR(16) PRIMARY KEY
 );
+
+-- ALTER TABLE PER AGGIUNGERE IL RIFERIMENTO ALLA TABELLA prenotazioni stanze
+-- precedentemente creata
+ALTER TABLE `Prenotazione stanze`
+ADD FOREIGN KEY (utente) REFERENCES `Utente`(`codice carta`);
+
 CREATE TABLE `Utente registrato` (
+`utente` 		CHAR(16) PRIMARY KEY,
+`nome`			VARCHAR(100) NOT NULL,
+`cognome` 		VARCHAR(100) NOT NULL,
+`indirizzo`		VARCHAR(100) NOT NULL,
+`codice documento` CHAR(20) NOT NULL,
 
-
+--Chiave esterna utente riferita a Utente
+FOREIGN KEY (utente) REFERENCES `Utente`(`codice carta`)
 );
+ 
 CREATE TABLE `Prenotazione escursione` (
+`codice utente` CHAR(16) NOT NULL,
+`codice escursione` INT UNSIGNED,
 
+PRIMARY KEY(`codice utente`,`codice escursione`),
 
+--Chiave esterna utente riferita a Utente
+FOREIGN KEY (utente) REFERENCES `Utente`(`codice carta`)
 );
+
 CREATE TABLE `Escursione` (
-
-
+`id`	 				INT UNSIGNED PRIMARY KEY,
+`inizio escursione` 	TIMESTAMP
 );
+
+-- ALTER TABLE PER AGGIUNGERE IL RIFERIMENTO ALLA TABELLA prenotazione escursione
+-- precedentemente creata
+ALTER TABLE `Prenotazione escursione`
+ADD FOREIGN KEY (`codice escursione`) REFERENCES `Escursione`(`id`);
+
 CREATE TABLE `Composizione escursione` (
+`nome zona` 			CHAR(70) NOT NULL,
+`agriturismo` 			INT UNSIGNED NOT NULL,
+`escursione` 			INT UNSIGNED NOT NULL,
+`progressivo passaggio` TINYINT UNSIGNED NOT NULL,
+`tempo permanenza`	    SMALLINT UNSIGNED NOT NULL,
 
+PRIMARY KEY pk1(`nome zona`,`agriturismo`,`escursione`,`progressivo passaggio`)
 
+FOREIGN KEY (`escursione`) REFERENCES `Escursione`(`id`)
 );
 CREATE TABLE `Zona` (
+`nome` CHAR(70) 			NOT NULL,
+`agriturismo`  				INT UNSIGNED NOT NULL,
 
+PRIMARY KEY 				pk1(`nome`,`agriturismo`),
 
+FOREIGN KEY (`agriturismo`) REFERENCES `Agriturismo`(`id`)
 );
+
+-- ALTER TABLE PER AGGIUNGERE IL RIFERIMENTO ALLA TABELLA prenotazione escursione
+-- precedentemente creata
+ALTER TABLE `Composizione escursione`
+ADD FOREIGN KEY (`nome zona`,agriturismo) REFERENCES `Escursione`(`nome`,agriturismo);
+
+
 CREATE TABLE `Transazione` (
 
 
